ed5ae560060e285d12b010e0a0638267a0151ade
RadhoineUlyss <43170329+RadhoineUlyss@users.noreply.github.com>
2018-11-13 14:27:21 +0100

    profil view

--- application/views/profil_view.php
+++ application/views/profil_view.php
@@ -28,21 +28,21 @@ echo "<p>Compte créé le " . $d->format("d-m-Y") . " .</p>" ?>
         <th scope="row">Nom :</th>
         <td><?php echo $nom ?></td>
         <td>
-            <button id="nom" type="button" class="btn btn-outline-dark">Modifier</button>
+            <button name="btn_modif" id="nom" type="button" class="btn btn-outline-dark">Modifier</button>
         </td>
     </tr>
     <tr>
         <th scope="row">Prénom :</th>
         <td><?php echo $prenom ?></td>
         <td>
-            <button id="prenom" type="button" class="btn btn-outline-dark">Modifier</button>
+            <button name="btn_modif" id="prenom" type="button" class="btn btn-outline-dark">Modifier</button>
         </td>
     </tr>
     <tr>
         <th scope="row">Adresse :</th>
         <td><?php echo $code_postal . " " . $ville . " " . $ligne_adresse ?></td>
         <td>
-            <button id="adresse" type="button" class="btn btn-outline-dark" disabled>Modifier</button>
+            <button name="btn_modif" id="adresse" type="button" class="btn btn-outline-dark" disabled>Modifier</button>
         </td>
     </tr>
 </table>
@@ -53,41 +53,38 @@ echo "<p>Compte créé le " . $d->format("d-m-Y") . " .</p>" ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript">
 
-	$(document).ready(function (e) {
-		$('td > .btn').click(function () {
-			var id        = $(this).attr('id');
-			var parent    = $(this).parent().parent();
-			var titre     = parent.children().eq(0);
-			var valeur    = parent.children().eq(1);
-			var input_val = parent.children().eq(1);
-			var bouton    = parent.children().eq(2);
-			parent.append("<form method=\"post\" action=\"\" id=\"info_compte\">" + "</form>");
-			input_val.html("<input id=\"nom\" type=\"text\" form=\"info_compte\" name=\"" + id + "\" value=\"" + valeur.text() + "\">");
-			bouton.html("<td><button id=\"\" type=\"submit\" form=\"info_compte\" class=\"btn btn-dark\">Accepter</button></td>");
+	$('td>.btn').click(function () {
+		var id        = $(this).attr('id');
+		var parent    = $(this).parent().parent();
+		var titre     = parent.children().eq(0);
+		var valeur    = parent.children().eq(1);
+		var input_val = parent.children().eq(1);
+		var bouton    = parent.children().eq(2);
+		parent.append("<form method=\"post\" action=\"\" id=\"info_compte\">" + "</form>");
+		input_val.html("<input id=\"nom\" type=\"text\" form=\"info_compte\" name=\"" + id + "\" value=\"" + valeur.text() + "\">");
+		bouton.html("<td><button id=\"\" type=\"submit\" form=\"info_compte\" class=\"btn btn-dark\">Accepter</button></td>");
 
-			$('#info_compte').submit(function () {
-				$.ajax({
-					url: '<?php echo base_url() . 'profil/modifier/' ?>',
-					type: 'POST',
-					data: $(this).serialize(),
-					dataType: 'json',
-					success: function (data) {
-						if (data.status === "succes") {
-							parent.children().eq(1).text(data.var);
-							bouton.html("<button id=\"" + id + "\" type=\"button\" class=\"btn btn-outline-dark\">Modifier</button>");
-							// $( "#test_array" ).load( "profil_view.php #test_array" );
-						}
-						else {
-							console.log(data.nom);
-						}
-					},
-					error: function (data) {
-						console.log(this.data);
+		$('#info_compte').submit(function () {
+			$.ajax({
+				url: '<?php echo base_url() . 'profil/modifier/' ?>',
+				type: 'POST',
+				data: $(this).serialize(),
+				dataType: 'json',
+				success: function (data) {
+					if (data.status === "succes") {
+						parent.children().eq(1).text(data.var);
+						bouton.html("<button id=\"" + id + "\" type=\"button\" class=\"btn btn-outline-dark\">Modifier</button>");
+						// $( "#test_array" ).load( "profil_view.php #test_array" );
 					}
-				});
-				return false;
+					else {
+						console.log(data.nom);
+					}
+				},
+				error: function (data) {
+					console.log(this.data);
+				}
 			});
-
+			return false;
 		});
 
 	});
