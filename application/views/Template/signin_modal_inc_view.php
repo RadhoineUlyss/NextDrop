ac0eeb9adbf2d217baed1148118a1350042ce717
Loïc <lolo.mess.17@gmail.com>
2019-01-08 17:55:05 +0100

    minor changes

--- application/views/Template/signin_modal_inc_view.php
+++ application/views/Template/signin_modal_inc_view.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<div class="modal-dialog modal-login">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h4 class="modal-title">Connection</h4>
+				<h4 class="modal-title">Connexion</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 			</div>
 			<div class="modal-body">
