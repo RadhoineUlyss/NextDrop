4005f6304c361029e59460b99350b8e4a523220b
Loïc <lolo.mess.17@gmail.com>
2019-01-15 01:17:45 +0100

    Added a favicon / minor changes in the footer

--- application/views/Template/template.php
+++ application/views/Template/template.php
@@ -14,6 +14,25 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Font -->
     <link type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,700"/>
+    <!-- FavIcon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url(); ?>assets/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url(); ?>assets/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>assets/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>assets/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>assets/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url(); ?>assets/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url(); ?>assets/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url(); ?>assets/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>assets/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo base_url(); ?>assets/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url(); ?>assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo base_url(); ?>assets/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php echo base_url(); ?>assets/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
     <?php
     if ($style != '') {
         echo "<link rel=\"stylesheet\" href=" . base_url() . "assets/custom/css/" . $style . ".css>";
@@ -126,14 +145,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
                 <!-- Links -->
-                <h6 class="text-uppercase font-weight-bold">Nos produits</h6>
-                <p>
+                <h6 class="text-uppercase font-weight-bold">Catégories</h6>
+                <p class="footerSecText">
                     <a href="<?php echo base_url('/sneakers') ?>">SNEAKERS</a>
                 </p>
-                <p>
+                <p class="footerSecText">
                     <a href="#!">STREETWEAR</a>
                 </p>
-                <p>
+                <p class="footerSecText">
                     <a href="#!">ACCESSOIRES</a>
                 </p>
 
@@ -145,13 +164,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
                 <!-- Links -->
                 <h6 class="text-uppercase font-weight-bold">Liens utiles</h6>
-                <p>
+                <p class="footerSecText">
                     <a href="#!">VOTRE COMPTE</a>
                 </p>
-                <p>
-                    <a href="#!">ABOUT US</a>
+                <p class="footerSecText">
+                    <a href="#!">A PROPOS</a>
                 </p>
-                <p>
+                <p class="footerSecText">
                     <a href="#!">CONTACT</a>
                 </p>
 
