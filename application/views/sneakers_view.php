852c2805917b967acf9599b43773a4f46b1fce3e
Loïc <lolo.mess.17@gmail.com>
2019-02-26 17:33:02 +0100

    template galerie

--- application/views/sneakers_view.php
+++ application/views/sneakers_view.php
@@ -1,23 +1,25 @@
-<h1> Les derniers articles</h1>
-
-<p>La liste :</p>
-
 <!--<?php
     var_dump($sneakers);
 ?>-->
 
-<div id="galerie" class="container">
-    <div class="x1y1"></div>
-    <div class="x2y1"></div>
-    <div class="x3y1"></div>
-    <div class="x4y1"></div>
-
-    <div class="x1y2"></div>
-    <div class="x2y2"></div>
-    <div class="x3y2"></div>
-
-    <div class="x1y3"></div>
-    <div class="x2y3"></div>
-    <div class="x3y3"></div>
-    <div class="x4y3"></div>
+<div id="galerie">
+    <div class="containersneak">
+        <div class="firstRow">
+            <div class="menuDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+        </div>
+        <div class="vidRow">
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="videoDiv"></div>
+        </div>
+        <div class="baseRow">
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+        </div>
+    </div>
 </div>
