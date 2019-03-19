b9744f6c2f9a2c1b9d2dc61b7d28d1562184ed98
Loïc <lolo.mess.17@gmail.com>
2019-03-19 17:22:16 +0100

    item display in gallery

--- application/views/sneakers_view.php
+++ application/views/sneakers_view.php
@@ -1,25 +1,10 @@
-<?php
-#var_dump($sneakers);
-?>
+<!-- <?php foreach ($sneakers as $sneack) { ?>
+    <div class="articlebox col-lg-3 col-md-4 col-xs-6" style="border:1px solid black;margin: 10px;">
+        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+        <p style="text-align: left"><?php echo $sneack->nom ?> </p>
+    </div>
+<?php } ?> -->
 
-<!-- <div id="galerie" class="row text-center text-lg-left">
-
-    <?php foreach ($sneakers as $sneack) { ?>
-        <div class="articlebox col-lg-3 col-md-4 col-xs-6" style="border:1px solid black;margin: 10px;">
-            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-            <p style="text-align: left"><?php echo $sneack->nom ?> </p>
-        <div>
-                <?php echo  $sneack->prix ?>
-                <?php /*for($i =0; $i< count($sneack->tailles);$i++) {
-                    if($sneack->tailles[$i] != 0)
-                        echo '<span>'.$sneack->tailles{$i}.'</span>';
-                    else
-                        echo '<span>Unique</span>';
-                }*/?>
-            </div>
-        </div>
-    <?php } ?>
-</div> -->
 
 <br id="brspec">
 <div id="galerie">
@@ -33,7 +18,11 @@
                     <li>- YEEZY</li>
                 </ul>
             </div>
-            <div class="itemDiv"></div>
+            <div class="itemDiv">
+                <img src="http://placehold.it/400x300" alt="NomSneaker">
+                <span class="SneakName">Test</span><br>
+                <span class="SneakPrice">230€</span>
+            </div>
             <div class="itemDiv"></div>
             <div class="itemDiv"></div>
         </div>
