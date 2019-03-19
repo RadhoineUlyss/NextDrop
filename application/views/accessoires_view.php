e0d1bb3e4def8194ec9f1d742fecf1680ab98ada
lorenzochaudessolle <lorenzo.chaudessolle@ynov.com>
2019-03-19 17:24:58 +0100

    ajout accessoire

---
+++ application/views/accessoires_view.php
@@ -0,0 +1,53 @@
+<?php
+#var_dump($sneakers);
+?>
+
+<!-- <div id="galerie" class="row text-center text-lg-left">
+
+    <?php foreach ($accessoires as $accessoire) { ?>
+        <div class="articlebox col-lg-3 col-md-4 col-xs-6" style="border:1px solid black;margin: 10px;">
+            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+            <p style="text-align: left"><?php echo $accessoire->nom ?> </p>
+        <div>
+                <?php echo  $accessoire->prix ?>
+                <?php /*for($i =0; $i< count($sneack->tailles);$i++) {
+                    if($sneack->tailles[$i] != 0)
+                        echo '<span>'.$sneack->tailles{$i}.'</span>';
+                    else
+                        echo '<span>Unique</span>';
+                }*/?>
+            </div>
+        </div>
+    <?php } ?>
+</div> -->
+
+<br id="brspec">
+<div id="galerie">
+    <div class="containersneak">
+        <div class="firstRow">
+            <div class="menuDiv">
+                <h4>FILTRER :</h4>
+                <ul>
+                    <li>- NIKE</li>
+                    <li>- ADIDAS</li>
+                    <li>- YEEZY</li>
+                </ul>
+            </div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+        </div>
+        <div class="vidRow">
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="videoDiv"></div>
+        </div>
+        <div class="baseRow">
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+            <div class="itemDiv"></div>
+        </div>
+    </div>
+</div>
+<br>
