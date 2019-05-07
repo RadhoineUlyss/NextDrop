787d2b7b06f6b0fd1a605e13e1ddbab3016d7472
lorenzochaudessolle <lorenzo.chaudessolle@ynov.com>
2019-05-07 15:31:58 +0200

    tailles article

--- application/views/article_view.php
+++ application/views/article_view.php
@@ -10,22 +10,32 @@
 //var_dump($article);
 ?>
 
-<p>Je suis un article</p>
 <p><?php echo $article->nom ?></p>
 <p><?php echo $article->marque ?></p>
 <p><?php echo $article->reference ?></p>
 <p><?php echo $article->date_sortie ?></p>
 <p><?php echo $article->type ?></p>
 <p><?php echo $article->prix ?></p>
-<p><?php echo $article->taille ?></p>
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+    <?php for ($i = 0; $i < count($article->tailles); $i++) {
+        if ($article->tailles[$i] != 0)
+            echo ' <label class="btn btn-dark">
+                    <input type="radio" name="options" autocomplete="off">'
+                    .$article->tailles{$i}.'
+                  </label>';
+        else
+            echo '<span>Unique</span>';
+    }
+    ?>
+</div>
 <p><?php echo $article->couleur ?></p>
+<button type="button" class="btn btn-outline-dark">Acheter</button>
 <?php
-var_dump($article);
+//var_dump($article);
 /*for($i =0; $i< count($article->tailles);$i++) {
                     if($article->tailles[$i] != 0)
                         echo '<span>'.$article->tailles{$i}.'</span>';
                     else
                         echo '<span>Unique</span>';
-                }*/?>
-<button type="button" class="btn btn-outline-dark">Acheter</button>
+                }*/ ?>
 <br>
